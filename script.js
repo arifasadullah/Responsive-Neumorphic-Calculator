9d576923f4be5e08f233991a5d278f55ec95377b
arifasadullah123 <arifasadullah123@gmai.com>
2022-01-11 01:33:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let outputScreen = document.getElementById("input");
+
+function display(num) {
+  outputScreen.value += num;
+}
+
+function Calculate() {
+  try {
+    outputScreen.value = eval(outputScreen.value);
+  } catch (err) {
+    alert("Invalid Input");
+  }
+}
+
+function Clear() {
+  outputScreen.value = "";
+}
+
+function del() {
+  outputScreen.value = outputScreen.value.slice(0, -1);
+}
